d8125f482e05a038020149f2087d5c6405aaaf55
jusang <735409556@qq.com>
2021-03-02 16:46:18 +0800

    [修改] 鉴权功能的dao层和service层

--- src/main/java/com/cl/fun/gag/pic/dao/UserMapper.java
+++ src/main/java/com/cl/fun/gag/pic/dao/UserMapper.java
@@ -2,7 +2,9 @@ package com.cl.fun.gag.pic.dao;
 
 import com.cl.fun.gag.pic.entity.UserPo;
 import com.cl.fun.gag.pic.entity.auth.Role;
+import com.cl.fun.gag.pic.entity.sql.UmsPermission;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -13,4 +15,7 @@ public interface UserMapper {
     List<Role> getRoleListByUsername(String username);
 
     int insertUser(UserPo userPo);
+
+    List<UmsPermission> getUserPermission(@Param("userId") Integer userId);
+
 }
