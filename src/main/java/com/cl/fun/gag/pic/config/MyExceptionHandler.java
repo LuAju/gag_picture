b5c7badff9bc9c74562dfd09fe0c1bbc545668c6
jusang <735409556@qq.com>
2021-03-03 13:39:34 +0800

    [修改] 增加了用户名重复的异常捕获

--- src/main/java/com/cl/fun/gag/pic/config/MyExceptionHandler.java
+++ src/main/java/com/cl/fun/gag/pic/config/MyExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.cl.fun.gag.pic.config;
 
 import com.cl.fun.gag.pic.common.result.CommonResult;
+import com.cl.fun.gag.pic.customizeexception.NameDuplicateException;
 import com.cl.fun.gag.pic.customizeexception.SizeZeroException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class MyExceptionHandler {
 
 //    @ExceptionHandler(value =Exception.class)
@@ -16,9 +19,11 @@ public class MyExceptionHandler {
 
     @ExceptionHandler(value = SizeZeroException.class)
     public Object exceptionHandler(Exception e) {
-        System.out.println("未知异常！原因是:" + e);
         return CommonResult.error(e.getMessage());
     }
 
-
+    @ExceptionHandler(value = NameDuplicateException.class)
+    public Object NameDuplicateExceptionHandler(Exception e) {
+        return CommonResult.error("注册失败，用户名重复");
+    }
 }
