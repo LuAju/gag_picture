58c6840dfe17de82563a7320d9c343c63a7fe135
ju <735409556@qq.com>
2021-07-11 18:38:20 +0800

    [修改] 1。 用户请求，同步日志功能；2. 跨域配置

--- src/main/java/com/cl/fun/gag/pic/controller/PicSearchController.java
+++ src/main/java/com/cl/fun/gag/pic/controller/PicSearchController.java
@@ -10,6 +10,7 @@ import com.cl.fun.gag.pic.service.PicService;
 import com.cl.fun.gag.pic.utils.EntityConverter;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,27 +19,28 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/search")
-
+@Slf4j
 public class PicSearchController {
     @Autowired
     private PicService picService;
 
-    @ApiOperation("通过图片名称简单搜索" )
+    @ApiOperation("通过图片名称简单搜索")
     @PostMapping("/getPicsByName")
     @SysLog
     @SearchCounter
-    public Object getPicsByName(@RequestBody PicNameSearchDto picNameSearchDto){
+    public Object getPicsByName(@RequestBody PicNameSearchDto picNameSearchDto) {
         // TODO 将搜索添加打redis里
         List<PicturePo> picListByPicName = picService.getPicListByPicName(picNameSearchDto.getPicName(), picNameSearchDto.getPage(), picNameSearchDto.getSize());
+        log.info("用户搜索{}，返回{}条数据", picNameSearchDto.getPicName(), picListByPicName.size());
         return CommonResult.success(picListByPicName);
     }
 
     @ApiOperation("通过图片的主键ID查询")
     @GetMapping("/getPicByPrimaryKey/{id}")
     @SysLog
-    public Object getPicByPrimaryKey(@PathVariable("id") String id){
+    public Object getPicByPrimaryKey(@PathVariable("id") String id) {
         PicturePo picDetail = picService.getPicDetail(Long.valueOf(id));
-        if (picDetail!=null) {
+        if (picDetail != null) {
             return CommonResult.success(EntityConverter.picturePo2Dto(picDetail));
         } else {
             return CommonResult.fail("找不到对应数据");
@@ -49,18 +51,44 @@ public class PicSearchController {
     @PostMapping("/getPicsByDetail")
     @SearchCounter
     @SysLog
-    public Object getPicsByDetail(@RequestBody PicDetailSearchDto picDetailSearchDto){
+    public Object getPicsByDetail(@RequestBody PicDetailSearchDto picDetailSearchDto) {
         // TODO 将搜索添加打redis里
         List<PicturePo> picListByPicDetail = picService.getPicListByPicDetail(picDetailSearchDto.getPicDetail(), picDetailSearchDto.getPage(), picDetailSearchDto.getSize());
+        log.info("用户搜索{}，返回{}条数据", picDetailSearchDto.getPicDetail(), picListByPicDetail.size());
+        return CommonResult.success(picListByPicDetail);
+    }
+
+//    @ApiOperation("根据图片详情获取")
+//    @PostMapping("/getPicsByDetailOrName")
+//    @SearchCounter
+//    @SysLog
+//    public Object getPicsByDetailOrName(@RequestBody PicDetailSearchDto picDetailSearchDto) {
+//        // TODO 将搜索添加打redis里
+//        List<PicturePo> picListByPicDetail = picService.getPicListByPicDetail(picDetailSearchDto.getPicDetail(), picDetailSearchDto.getPage(), picDetailSearchDto.getSize());
+//        log.info("用户搜索{}，返回{}条数据", picDetailSearchDto.getPicDetail(), picDetailSearchDto.getSize());
+//        return CommonResult.success(picListByPicDetail);
+//    }
+
+    @ApiOperation("根据图片详情获取")
+    @PostMapping("/getPicsByDetailOrName")
+    @SearchCounter
+    @SysLog
+    public Object getPicListByPicDetailOrName(@RequestBody PicDetailSearchDto picDetailSearchDto) {
+        // TODO 将搜索添加打redis里
+        List<PicturePo> picListByPicDetail = picService.getPicListByPicDetailOrName(picDetailSearchDto.getPicDetail(), picDetailSearchDto.getPage(), picDetailSearchDto.getSize());
+        log.info("用户搜索{}，返回{}条数据", picDetailSearchDto.getPicDetail(), picListByPicDetail.size());
         return CommonResult.success(picListByPicDetail);
     }
 
     @ApiOperation("搜索热度")
     @GetMapping("/getTopPicList")
-    public Object getTopPicList(){
+    public Object getTopPicList() {
         // TODO 从redis里获取搜索次数
         List<String> topId = new ArrayList<>();
         List<PicturePo> topPicList = picService.getTopPicList(topId);
         return CommonResult.success(topPicList);
     }
+
+    // todo 今日新图
+    // 根据时间显示新添加的图 最好从redis里获取
 }
